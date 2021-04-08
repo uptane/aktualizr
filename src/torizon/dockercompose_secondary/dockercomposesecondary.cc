f43db5bdb46d893be9e0e512115f71a6729e586b
Jeremias Cordoba <jeremias.cordoba@toradex.com>
2021-04-08 11:20:22 -0700

    Initial support for docker compose secondaries
    Signed-off-by: Phil Wise <phil@phil-wise.com>
    Signed-off-by: Stefan Agner <stefan.agner@toradex.com>
    Signed-off-by: Ming Liu <ming.liu@toradex.com>
    Signed-off-by: Jeremias Cordoba <jeremias.cordoba@toradex.com>

---
+++ src/torizon/dockercompose_secondary/dockercomposesecondary.cc
@@ -0,0 +1,89 @@
+#include "dockercomposesecondary.h"
+#include "uptane/manifest.h"
+#include "libaktualizr/types.h"
+#include "logging/logging.h"
+#include "utilities/fault_injection.h"
+#include "utilities/utils.h"
+
+#include <sstream>
+
+using std::stringstream;
+
+namespace Primary {
+
+const char* const DockerComposeSecondaryConfig::Type = "docker-compose";
+
+DockerComposeSecondaryConfig::DockerComposeSecondaryConfig(const Json::Value& json_config) : ManagedSecondaryConfig(Type) {
+  partial_verifying = json_config["partial_verifying"].asBool();
+  ecu_serial = json_config["ecu_serial"].asString();
+  ecu_hardware_id = json_config["ecu_hardware_id"].asString();
+  full_client_dir = json_config["full_client_dir"].asString();
+  ecu_private_key = json_config["ecu_private_key"].asString();
+  ecu_public_key = json_config["ecu_public_key"].asString();
+  firmware_path = json_config["firmware_path"].asString();
+  target_name_path = json_config["target_name_path"].asString();
+  metadata_path = json_config["metadata_path"].asString();
+}
+
+std::vector<DockerComposeSecondaryConfig> DockerComposeSecondaryConfig::create_from_file(
+    const boost::filesystem::path& file_full_path) {
+  Json::Value json_config;
+  std::ifstream json_file(file_full_path.string());
+  Json::parseFromStream(Json::CharReaderBuilder(), json_file, &json_config, nullptr);
+  json_file.close();
+
+  std::vector<DockerComposeSecondaryConfig> sec_configs;
+  sec_configs.reserve(json_config[Type].size());
+
+  for (const auto& item : json_config[Type]) {
+    sec_configs.emplace_back(DockerComposeSecondaryConfig(item));
+  }
+  return sec_configs;
+}
+
+void DockerComposeSecondaryConfig::dump(const boost::filesystem::path& file_full_path) const {
+  Json::Value json_config;
+
+  json_config["partial_verifying"] = partial_verifying;
+  json_config["ecu_serial"] = ecu_serial;
+  json_config["ecu_hardware_id"] = ecu_hardware_id;
+  json_config["full_client_dir"] = full_client_dir.string();
+  json_config["ecu_private_key"] = ecu_private_key;
+  json_config["ecu_public_key"] = ecu_public_key;
+  json_config["firmware_path"] = firmware_path.string();
+  json_config["target_name_path"] = target_name_path.string();
+  json_config["metadata_path"] = metadata_path.string();
+
+  Json::Value root;
+  root[Type].append(json_config);
+
+  Json::StreamWriterBuilder json_bwriter;
+  json_bwriter["indentation"] = "\t";
+  std::unique_ptr<Json::StreamWriter> const json_writer(json_bwriter.newStreamWriter());
+
+  boost::filesystem::create_directories(file_full_path.parent_path());
+  std::ofstream json_file(file_full_path.string());
+  json_writer->write(root, &json_file);
+  json_file.close();
+}
+
+DockerComposeSecondary::DockerComposeSecondary(Primary::DockerComposeSecondaryConfig sconfig_in)
+    : ManagedSecondary(std::move(sconfig_in)) {}
+
+bool DockerComposeSecondary::getFirmwareInfo(Uptane::InstalledImageInfo& firmware_info) const {
+  std::string content;
+
+  if (!boost::filesystem::exists(sconfig.target_name_path) || !boost::filesystem::exists(sconfig.firmware_path)) {
+    firmware_info.name = std::string("noimage");
+    content = "";
+  } else {
+    firmware_info.name = Utils::readFile(sconfig.target_name_path.string());
+    content = Utils::readFile(sconfig.firmware_path.string());
+  }
+  firmware_info.hash = Uptane::ManifestIssuer::generateVersionHashStr(content);
+  firmware_info.len = content.size();
+
+  return true;
+}
+
+}  // namespace Primary
---
+++ src/torizon/dockercompose_secondary/dockercomposesecondary.h
@@ -0,0 +1,41 @@
+#ifndef PRIMARY_DOCKERCOMPOSESECONDARY_H_
+#define PRIMARY_DOCKERCOMPOSESECONDARY_H_
+
+#include <string>
+#include "managedsecondary.h"
+#include "libaktualizr/types.h"
+
+namespace Primary {
+
+class DockerComposeSecondaryConfig : public ManagedSecondaryConfig {
+ public:
+  DockerComposeSecondaryConfig() : ManagedSecondaryConfig(Type) {}
+  DockerComposeSecondaryConfig(const Json::Value& json_config);
+
+  static std::vector<DockerComposeSecondaryConfig> create_from_file(const boost::filesystem::path& file_full_path);
+  void dump(const boost::filesystem::path& file_full_path) const;
+
+ public:
+  static const char* const Type;
+};
+
+/**
+ * An primary secondary that runs on the same device but treats
+ * the firmware that it is pushed as a docker-compose yaml file
+ */
+class DockerComposeSecondary : public ManagedSecondary {
+ public:
+  explicit DockerComposeSecondary(Primary::DockerComposeSecondaryConfig sconfig_in);
+  ~DockerComposeSecondary() override = default;
+
+  std::string Type() const override { return DockerComposeSecondaryConfig::Type; }
+
+  bool ping() const override { return true; }
+
+ private:
+  bool getFirmwareInfo(Uptane::InstalledImageInfo& firmware_info) const override;
+};
+
+}  // namespace Primary
+
+#endif  // PRIMARY_DOCKERCOMPOSESECONDARY_H_
