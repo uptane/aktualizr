56025537dc7d8575f3b7a1d0ee58b38dd5ddf7ea
Rogerio Guerra Borin <rogerio.borin@toradex.com>
2022-08-15 12:56:17 -0300

    Change aktualizr-version string
    Change script responsible for determining the version string of
    aktualizr. Now the version string will have the form
    'tdx-<commit-hash>' possibly followed by the string '-dirty' (if
    there are uncommited changes in the project directory).
    
    Related-to: TOR-2266
    
    Signed-off-by: Rogerio Guerra Borin <rogerio.borin@toradex.com>

--- scripts/get_version.sh
+++ scripts/get_version.sh
@@ -5,4 +5,16 @@ set -euo pipefail
 GIT=${1:-git}
 REPO=${2:-.}
 
-"$GIT" -C "$REPO" describe --long | tr -d '\n'
+# TODO: [TDX] Consider a more user friendly versioning scheme.
+HASH=$("$GIT" -C "$REPO" rev-parse --short HEAD)
+RES1=$?
+if [ $RES1 -eq 0 ]; then
+    DIRTY=""
+    if ! "$GIT" -C "$REPO" diff --quiet || ! "$GIT" -C "$REPO" diff --cached --quiet; then
+        DIRTY="-dirty"
+    fi
+    echo -n "tdx-${HASH}${DIRTY}"
+    exit 0
+fi
+
+exit $RES1
