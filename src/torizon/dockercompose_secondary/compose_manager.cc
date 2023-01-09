115720dd8535891778ee70de353ef1c16dba1249
Eduardo Ferreira <eduardo.barbosa@toradex.com>
2023-01-09 15:12:19 -0300

    docker-compose: add '--wait' flag for healthchecks
    Now that we have docker compose v2, we can make use of the '--wait' flag
    to improve our container healthchecks.
    
    Related-to: TOR-2728
    
    Signed-off-by: Eduardo Ferreira <eduardo.barbosa@toradex.com>

--- src/torizon/dockercompose_secondary/compose_manager.cc
+++ src/torizon/dockercompose_secondary/compose_manager.cc
@@ -20,7 +20,7 @@ bool ComposeManager::pull(const std::string &compose_file) {
 
 bool ComposeManager::up(const std::string &compose_file) {
   LOG_INFO << "Running docker-compose up";
-  return cmd.run(compose_cmd_ + compose_file + " -p torizon up --detach --remove-orphans");
+  return cmd.run(compose_cmd_ + compose_file + " -p torizon up --detach --remove-orphans --wait");
 }
 
 bool ComposeManager::down(const std::string &compose_file) {
