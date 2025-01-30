f5f0a1263f17600f9bb78b5930debeb4a9d30225
Rogerio Guerra Borin <rogerio.borin@toradex.com>
2025-01-29 21:58:48 -0300

    Modify test to cover pushing commitmeta objects to TreeHub
    Update the test responsible for checking the "Upload to TreeHub"
    functionality so as to ensure the commitmeta object is also pushed to
    the server along with the corresponding commit.
    
    Signed-off-by: Rogerio Guerra Borin <rogerio.borin@toradex.com>

--- src/sota_tools/deploy_test.cc
+++ src/sota_tools/deploy_test.cc
@@ -17,29 +17,30 @@ TemporaryDirectory temp_dir;
 /* Fetch OSTree objects from source repository and push to destination repository.
  * Parse OSTree object to identify child objects. */
 TEST(deploy, UploadToTreehub) {
-  OSTreeRepo::ptr src_repo = std::make_shared<OSTreeDirRepo>("tests/sota_tools/repo");
+  OSTreeRepo::ptr src_repo = std::make_shared<OSTreeDirRepo>("tests/sota_tools/cmeta-repo");
   boost::filesystem::path filepath = (temp_dir.Path() / "auth.json").string();
   boost::filesystem::path cert_path = "tests/fake_http_server/server.crt";
   auto server_creds = ServerCredentials(filepath);
   auto run_mode = RunMode::kDefault;
   auto test_ref = src_repo->GetRef("master");
 
-  const uint8_t hash[32] = {0x16, 0xef, 0x2f, 0x26, 0x29, 0xdc, 0x92, 0x63, 0xfd, 0xf3, 0xc0,
-                            0xf0, 0x32, 0x56, 0x3a, 0x2d, 0x75, 0x76, 0x23, 0xbb, 0xc1, 0x1c,
-                            0xf9, 0x9d, 0xf2, 0x5c, 0x3c, 0x3f, 0x25, 0x8d, 0xcc, 0xbe};
+  const uint8_t hash[32] = {0x2d, 0xc5, 0xec, 0x3e, 0x8c, 0x87, 0xc1, 0x65, 0x30, 0x45, 0xdc,
+                            0xdb, 0x66, 0x37, 0x65, 0xad, 0x0b, 0xfb, 0x44, 0x91, 0x3f, 0x00,
+                            0x0a, 0xee, 0x9b, 0xe3, 0x7e, 0xa7, 0x3c, 0x0d, 0xe3, 0x80};
   TreehubServer push_server;
   EXPECT_EQ(authenticate(cert_path.string(), server_creds, push_server), EXIT_SUCCESS);
   UploadToTreehub(src_repo, push_server, OSTreeHash(hash), run_mode, 2, true);
 
   int result = system(
-      (std::string("diff -r ") + (temp_dir.Path() / "objects/").string() + " tests/sota_tools/repo/objects/").c_str());
+      (std::string("diff -rw ") + (temp_dir.Path() / "objects/").string() + " tests/sota_tools/cmeta-repo/objects/")
+          .c_str());
   EXPECT_EQ(result, 0) << "Diff between the source repo objects and the destination repo objects is nonzero.";
 
   bool push_root_ref_res = PushRootRef(push_server, test_ref);
   EXPECT_TRUE(push_root_ref_res);
 
-  result =
-      system((std::string("diff -r ") + (temp_dir.Path() / "refs/").string() + " tests/sota_tools/repo/refs/").c_str());
+  result = system(
+      (std::string("diff -rw ") + (temp_dir.Path() / "refs/").string() + " tests/sota_tools/cmeta-repo/refs/").c_str());
   EXPECT_EQ(result, 0) << "Diff between the source repo refs and the destination repos refs is nonzero.";
 }
 
