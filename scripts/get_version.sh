736326f44b028ce05b5104b6453486858cac3df2
Leonardo Held <leonardoheld@protonmail.com>
2025-02-17 10:08:34 -0300

    Use the short hash instead of version if tags are not available during build
    This change is specifically done so that the main build action on bullseye
    doesn't fail on forks.
    
    Signed-off-by: Leonardo Held <leonardoheld@protonmail.com>

--- scripts/get_version.sh
+++ scripts/get_version.sh
@@ -5,4 +5,8 @@ set -euo pipefail
 GIT=${1:-git}
 REPO=${2:-.}
 
-"$GIT" -C "$REPO" describe --long | tr -d '\n'
+if ! VERSION=$("$GIT" -C "$REPO" describe --long 2>/dev/null | tr -d '\n'); then
+    VERSION=$("$GIT" -C "$REPO" rev-parse --short HEAD | tr -d '\n')
+fi
+
+printf "%s" "$VERSION"
