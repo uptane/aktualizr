0e7ff499a86bc454c92de2a04fd812f0261ae54f
Phil Wise <phil@phil-wise.com>
2025-09-28 14:44:49 +0100

    Move Debian Stable CI from Bookworm to Trixie
    The static checks are now using Trixie, disable them on Focal.

--- scripts/build-and-test-trixie.sh
+++ scripts/build-and-test-trixie.sh
@@ -5,8 +5,8 @@ set -euo pipefail
 # This expects to find the checked out source code in ./source
 # It builds it into ./build and runs the tests.
 # Test with
-# docker build -t aktualizr-bookworm -f docker/Dockerfile.debian.bookworm .
-# docker run --mount=type=volume,source=ccache,destination=/home/testuser/.cache -it aktualizr-bookworm source/scripts/build-and-test-bookworm.sh
+# docker build -t aktualizr-trixie -f docker/Dockerfile.debian.trixie .
+# docker run --mount=type=volume,source=ccache,destination=/home/testuser/.cache -it aktualizr-trixie source/scripts/build-and-test-trixie.sh
 cmake -G Ninja -S source -B build \
   -DBUILD_SOTA_TOOLS=ON \
   -DBUILD_OSTREE=OFF
